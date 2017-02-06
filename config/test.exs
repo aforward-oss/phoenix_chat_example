7382617fd93f8e318ba8afbd8502706b18a91382
Andrew Forward <aforward@gmail.com>
2017-02-06 13:48:39 -0500

    Enable .
    Finished in 0.02 seconds
    1 test, 0 failures
    
    Randomized with seed 8222

--- config/test.exs
+++ config/test.exs
@@ -6,5 +6,15 @@ config :chat, Chat.Endpoint,
   http: [port: 4001],
   server: false
 
+config :chat, Chat.Repo, [
+  adapter: Ecto.Adapters.Postgres,
+  database: "app_#{Mix.env}",
+  username: "postgres",
+  password: "",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox]
+
+
 # Print only warnings and errors during test
 config :logger, level: :warn
+
